24d77433aa2e2e1289bfe2a1c7ccb7c5b76d6a73
Pat Howell <patrick.howell@outlook.com>
2015-06-17 22:26:13 +0100

    Updated git ignore, created project, started implementing then got distracted.

---
+++ CssTagger/CssTagger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CssTagger
+{
+    public class CssTagger
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Count() != 3) 
+            {
+                PrintUsageMessage();
+            }
+            else 
+            {
+                var runner = new Runner();
+                var result = runner.RunTagging(args[0], args[1], args[2]);
+                Console.WriteLine(result);
+            }
+        }
+
+        private static void PrintUsageMessage()
+        {
+            Console.WriteLine("3 parameters are required: 1) Path to file containing keywords to locate, 2) String to tag keywords with, 3) Path to locate keywords in");
+        }
+    }
+}
---
+++ CssTagger/Runner.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CssTagger
+{
+    internal class Runner
+    {
+        /// <summary>
+        ///  Perform the task of running tagging keywords in a file.
+        /// </summary>
+        /// <param name="keywordsFile">Path to the file containing the keywords to locate</param>
+        /// <param name="markerWord">String to place, this is envisioned that this will be something unique that can be searched for easily</param>
+        /// <param name="fileToScan">Path to the file to search for keywords in and to add tag words to</param>
+        /// <returns>String status message about the final result of the tagging operation</returns>
+        public string RunTagging(string keywordsFile, string markerWord, string fileToScan)
+        {
+            var result = string.Empty;
+            try
+            {
+                var keywords = LoadFileContent(keywordsFile);
+                var scanFileContents = LoadFileContent(keywordsFile);
+            }
+            catch (Exception e)
+            {
+                result = e.Message;
+            }
+            return result;
+        }
+
+        private IEnumerable<string> LoadFileContent(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                return File.ReadAllLines(filePath);
+            }
+            else
+            {
+                throw new FileNotFoundException(string.Join(": ", "Unable to find file at path", filePath));
+            }
+        }
+
+        private IEnumerable<string> TagKeywordsInScanFileContents(IEnumerable<string> keywords, string markerWord, IEnumerable<string> scanContent)
+        {
+            var result = scanContent.Select(line => line.Contains())
+            return null;
+        }
+    }
+}
